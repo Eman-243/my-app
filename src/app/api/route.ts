909394c5e5292e0fb6437cf4e32dcae8e63d9627
Eman-243 <122217477+Eman-243@users.noreply.github.com>
2024-05-29 08:27:36 +0300

    .

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+import { products } from '@/data/productdata';
+
+export async function GET(productId: string) {
+    const product = products.find(product => product.id === productId);
+    return product;
+}
+//app/pages/productList/[categoryId]/[subcategoryId]/[productId]/route.ts
+
